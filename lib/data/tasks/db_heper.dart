2a7b722016dd535e97d9af17fd16abe1d4b82b0d
unknown <1803684g@umich.mx>
2023-07-23 01:10:30 -0500

    crud completo

--- lib/data/tasks/db_heper.dart
+++ lib/data/tasks/db_heper.dart
@@ -11,6 +11,50 @@ class SQHelper {
   }
 
   static Future<sql.Database> db() async {
-    return sql.openDatabase("database_name.db");
+    return sql.openDatabase("database_name.db", version: 1,
+        onCreate: (sql.Database database, int version) async {
+      await createTables(database);
+    });
+  }
+
+  static Future<int> createData(String title, String? desc) async {
+    final db = await SQHelper.db();
+
+    final data = {'title': title, 'desc': desc};
+    final id = await db.insert('data', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+
+    return id;
+  }
+
+  static Future<List<Map<String, dynamic>>> getAllData() async {
+    final db = await SQHelper.db();
+    return db.query('data', orderBy: 'id');
+  }
+
+  static Future<List<Map<String, dynamic>>> getSingleDta(int id) async {
+    final db = await SQHelper.db();
+    return db.query('data', where: "id = ?", whereArgs: [id], limit: 1);
+  }
+
+  static Future<int> updateData(int id, String title, String? desc) async {
+    final db = await SQHelper.db();
+    final data = {
+      'title': title,
+      'desc': desc,
+      'createdAt': DateTime.now().toString()
+    };
+
+    final result =
+        await db.update('data', data, where: "id = ?", whereArgs: [id]);
+    return result;
+  }
+
+  static Future<void> deleteData(int id) async {
+    final db = await SQHelper.db();
+    try {
+      await db.delete('data', where: "id =?", whereArgs: [id]);
+      // ignore: empty_catches
+    } catch (e) {}
   }
 }
--- lib/iu/pages/home/screen_home.dart
+++ lib/iu/pages/home/screen_home.dart
@@ -1,12 +1,168 @@
+import 'package:crud/data/tasks/db_heper.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, dynamic>> _allData = [];
+  bool _isLoading = true;
+
+  void _refreshData() async {
+    final data = await SQHelper.getAllData();
+    setState(() {
+      _allData = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshData();
+  }
+
+  Future<void> _addData() async {
+    await SQHelper.createData(_titleController.text, _descController.text);
+    _refreshData();
+  }
+
+  Future<void> _updateData(int id) async {
+    await SQHelper.updateData(id, _titleController.text, _descController.text);
+    _refreshData();
+  }
+
+  Future<void> _deteleData(int id) async {
+    await SQHelper.deleteData(id);
+    // ignore: use_build_context_synchronously
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      backgroundColor: Colors.red,
+      content: Text("Data detele"),
+    ));
+    _refreshData();
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descController = TextEditingController();
+
+  void showBottomSheet(int? id) async {
+    if (id != null) {
+      final existingData = _allData.firstWhere((element) => element[id] == id);
+      _titleController.text = existingData['title'];
+      _descController.text = existingData['desc'];
+    }
+
+    showModalBottomSheet(
+        elevation: 5,
+        isScrollControlled: true,
+        context: context,
+        builder: (_) => Container(
+              padding: EdgeInsets.only(
+                  top: 30,
+                  left: 15,
+                  right: 15,
+                  bottom: MediaQuery.of(context).viewInsets.bottom + 50),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Title"),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: _descController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), hintText: "Description"),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (id == null) {
+                          await _addData();
+                        }
+                        if (id != null) {
+                          await _updateData(id);
+                        }
+
+                        _titleController.text = "";
+                        _descController.text = "";
+
+                        // ignore: use_build_context_synchronously
+                        Navigator.of(context).pop();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(18),
+                        child: Text(
+                          id == null ? "Add Data" : "Update",
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("hola mundo screen"),
+      backgroundColor: const Color.fromARGB(240, 255, 255, 255),
+      appBar: AppBar(
+        title: const Text("Crud Operations force"),
+        centerTitle: true,
+      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _allData.length,
+              itemBuilder: (context, index) => Card(
+                margin: const EdgeInsets.all(15),
+                child: ListTile(
+                  title: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Text(
+                      _allData[index]['title'],
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  subtitle: Text(_allData[index]['desc']),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            _updateData(_allData[index]['id']);
+                          },
+                          icon: const Icon(Icons.edit, color: Colors.indigo)),
+                      IconButton(
+                          onPressed: () {
+                            _deteleData(_allData[index]['id']);
+                          },
+                          icon: const Icon(
+                            Icons.delete,
+                            color: Colors.redAccent,
+                          ))
+                    ],
+                  ),
+                ),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showBottomSheet(null),
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
